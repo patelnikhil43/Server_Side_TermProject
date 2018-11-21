7529cc20a6f9e6dd11d5bcea75246c0595359d1e
Nikhil <patelnikhil43@gmail.com>
2018-11-21 17:04:26 -0500

    Check if User Already exist while registering

--- TermProject/TermProjectSolution/TermProjectSolution/Registration.aspx.cs
+++ TermProject/TermProjectSolution/TermProjectSolution/Registration.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Utilities;
 
 namespace TermProjectSolution
 {
@@ -16,7 +19,38 @@ namespace TermProjectSolution
 
         protected void RegisterUserButton_Click(object sender, EventArgs e)
         {
+            //Validate
 
+            //Check if email already exist
+            String UserEmail = RegisterEmailTxtBox.Text;
+          Boolean flag =  CheckIfEmailExist(UserEmail);
+            if (flag == true)
+            {
+                //Alert User that email exist
+            }
+            else {
+                //Register 
+
+            }
+        }
+        public Boolean CheckIfEmailExist(String Email) {
+            DBConnect dbConnection = new DBConnect();
+            SqlCommand objCommand = new SqlCommand();
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "CheckIfUserExist"; 
+            SqlParameter inputParameter = new SqlParameter("@Email", Email);
+            inputParameter.Direction = ParameterDirection.Input;
+            inputParameter.SqlDbType = SqlDbType.NVarChar;
+            objCommand.Parameters.Add(inputParameter);
+
+            DataSet EmailDataSet = dbConnection.GetDataSetUsingCmdObj(objCommand);
+            if (EmailDataSet == null && EmailDataSet.Tables[0].Rows.Count == 0)
+            {
+                return false;
+            }
+            else {
+                return true;
+            }
         }
     }
 }
