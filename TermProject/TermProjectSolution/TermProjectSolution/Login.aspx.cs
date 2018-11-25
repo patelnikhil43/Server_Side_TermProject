d9a48c8ca10da58eeb246b14c74a5b696f058e29
Spencer <tuf89789@temple.edu>
2018-11-24 20:08:49 -0500

    added different login modes

--- TermProject/TermProjectSolution/TermProjectSolution/Login.aspx.cs
+++ TermProject/TermProjectSolution/TermProjectSolution/Login.aspx.cs
@@ -25,12 +25,73 @@ namespace TermProjectSolution
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            rdoNormalLogin.Checked = true;
             // Read encrypted password from cookie
             if (!IsPostBack && Request.Cookies["LoginCookie"] != null)
             {
                 HttpCookie myCookie = Request.Cookies["LoginCookie"];
-                txtEmail.Text = myCookie.Values["Username"];
-                txtPassword.Text = myCookie.Values["Password"];
+                //txtEmail.Text = myCookie.Values["Email"];
+                //txtPassword.Text = myCookie.Values["Password"];
+                String encryptedEmail = myCookie.Values["Email"];
+                String encryptedPassword = myCookie.Values["Password"];
+
+                Byte[] encryptedEmailBytes = Convert.FromBase64String(encryptedEmail);
+                Byte[] emailBytes;
+                String plainTextEmail;
+
+                UTF8Encoding encoder = new UTF8Encoding();
+
+                RijndaelManaged rmEncryption = new RijndaelManaged();
+                MemoryStream memStream = new MemoryStream();
+                CryptoStream decryptionStream = new CryptoStream(memStream, rmEncryption.CreateDecryptor(key, vector), CryptoStreamMode.Write);
+
+                //Email
+                decryptionStream.Write(encryptedEmailBytes, 0, encryptedEmailBytes.Length);
+                decryptionStream.FlushFinalBlock();
+
+                memStream.Position = 0;
+                emailBytes = new Byte[memStream.Length];
+                memStream.Read(emailBytes, 0, emailBytes.Length);
+
+                decryptionStream.Close();
+                memStream.Close();
+
+                plainTextEmail = encoder.GetString(emailBytes);
+                txtEmail.Text = plainTextEmail;
+
+                //Password
+                if (encryptedPassword != null)
+                {
+                    Byte[] encryptedPasswordBytes = Convert.FromBase64String(encryptedPassword);
+                    Byte[] passwordBytes;
+                    String plainTextPassword;
+
+                    memStream = new MemoryStream();
+                    decryptionStream = new CryptoStream(memStream, rmEncryption.CreateDecryptor(key, vector), CryptoStreamMode.Write);
+
+                    decryptionStream.Write(encryptedPasswordBytes, 0, encryptedPasswordBytes.Length);
+                    decryptionStream.FlushFinalBlock();
+
+                    memStream.Position = 0;
+                    passwordBytes = new Byte[memStream.Length];
+                    memStream.Read(passwordBytes, 0, passwordBytes.Length);
+
+                    decryptionStream.Close();
+                    memStream.Close();
+
+                    plainTextPassword = encoder.GetString(passwordBytes);
+                    txtPassword.Text = plainTextPassword;
+                    lblMessage.Text = "Password: " + plainTextPassword;
+                }
+
+                if (txtPassword.Text != "")
+                {
+                    rdoAutoLogin.Checked = true;
+                }
+                else
+                {
+                    rdoFastLogin.Checked = true;
+                }
             }
         }
 
@@ -63,11 +124,89 @@ namespace TermProjectSolution
 
                 DataSet myAccount = objDB.GetDataSetUsingCmdObj(objCommand);
 
+                //LOGIN SUCCESSFUL
                 if (myAccount.Tables[0].Rows.Count > 0)
                 {
                     lblMessage.Text = "You logged in good job!";
                     //might need to switch this to a UserAccount Object, not sure tho
                     Session.Add("userEmail", txtEmail.Text);
+
+                    String plainTextEmail = txtEmail.Text;
+                    String plainTextPassword = txtPassword.Text;
+                    String encryptedEmail;
+                    String encryptedPassword;
+
+                    UTF8Encoding encoder = new UTF8Encoding();
+                    Byte[] emailBytes;
+                    Byte[] passwordBytes;
+
+                    emailBytes = encoder.GetBytes(plainTextEmail);
+                    passwordBytes = encoder.GetBytes(plainTextPassword);
+
+                    RijndaelManaged rmEncryption = new RijndaelManaged();
+                    MemoryStream memStream = new MemoryStream();
+                    CryptoStream encryptionStream = new CryptoStream(memStream, rmEncryption.CreateEncryptor(key, vector), CryptoStreamMode.Write);
+ 
+                    if (rdoAutoLogin.Checked)
+                    {
+                        //Email
+                        encryptionStream.Write(emailBytes, 0, emailBytes.Length);
+                        encryptionStream.FlushFinalBlock();
+
+                        memStream.Position = 0;
+                        Byte[] encryptedEmailBytes = new byte[memStream.Length];
+                        memStream.Read(encryptedEmailBytes, 0, encryptedEmailBytes.Length);
+
+                        encryptionStream.Close();
+                        memStream.Close();
+
+                        //password
+                        memStream = new MemoryStream();
+                        encryptionStream = new CryptoStream(memStream, rmEncryption.CreateEncryptor(key, vector), CryptoStreamMode.Write);
+
+                        encryptionStream.Write(passwordBytes, 0, passwordBytes.Length);
+                        encryptionStream.FlushFinalBlock();
+
+                        memStream.Position = 0;
+                        Byte[] encryptedPasswordBytes = new byte[memStream.Length];
+                        memStream.Read(encryptedPasswordBytes, 0, encryptedPasswordBytes.Length);
+
+                        encryptionStream.Close();
+                        memStream.Close();
+
+                        encryptedEmail = Convert.ToBase64String(encryptedEmailBytes);
+                        encryptedPassword = Convert.ToBase64String(encryptedPasswordBytes);
+
+                        HttpCookie myCookie = new HttpCookie("LoginCookie");
+                        myCookie.Values["Email"] = encryptedEmail;
+                        myCookie.Expires = new DateTime(2020, 2, 1);
+                        myCookie.Values["Password"] = encryptedPassword;
+                        myCookie.Expires = new DateTime(2020, 2, 1);
+                        Response.Cookies.Add(myCookie);
+                    }
+                    else if (rdoFastLogin.Checked)
+                    {
+                        encryptionStream.Write(emailBytes, 0, emailBytes.Length);
+                        encryptionStream.FlushFinalBlock();
+
+                        memStream.Position = 0;
+                        Byte[] encryptedEmailBytes = new byte[memStream.Length];
+                        memStream.Read(encryptedEmailBytes, 0, encryptedEmailBytes.Length);
+
+                        encryptionStream.Close();
+                        memStream.Close();
+
+                        encryptedEmail = Convert.ToBase64String(encryptedEmailBytes);
+
+                        HttpCookie myCookie = new HttpCookie("LoginCookie");
+                        myCookie.Values["Email"] = encryptedEmail;
+                        myCookie.Expires = new DateTime(2020, 2, 1);
+                        Response.Cookies.Add(myCookie);
+                    }
+                    else
+                    {
+
+                    }
                 }
                 else
                 {
